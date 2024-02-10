7de062851594af93b2f0f38dd9a936e695e8ca80
steven <stevenman42@gmail.com>
2024-02-10 17:33:56 +1100

    finally got a little bit of javascript working

--- static/js/phasergame.js
+++ static/js/phasergame.js
@@ -1,46 +1,72 @@
-
 class Example extends Phaser.Scene
 {
-    preload ()
-    {
-        this.load.setBaseURL('https://labs.phaser.io');
-
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
-    }
-
     create ()
     {
-        this.add.image(400, 300, 'sky');
+        this.angle = 0;
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
+        this.graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x2266aa } });
+
+        this.point = new Phaser.Math.Vector2(250, 0);
+        this.point2 = new Phaser.Math.Vector2(250, 0);
+
+        this.text = this.add.text(30, 30, '');
+
+        this.input.on('pointermove', (pointer) => {
+
+            this.point2.copy(pointer);
+
+            this.point2.x -= 400;
+            this.point2.y -= 300;
+
         });
+    }
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+    update ()
+    {
+        const graphics = this.graphics;
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+        graphics.clear();
 
-        particles.startFollow(logo);
+        this.angle += 0.005;
+
+        //  Vector starting at 0/0
+        this.point.set(Math.cos(this.angle) * 250, Math.sin(this.angle) * 250);
+
+        // drawn from the center (as if center was 0/0)
+        graphics.lineBetween(400, 300, 400 + this.point.x, 300 + this.point.y);
+
+        graphics.lineStyle(2, 0x00aa00);
+        graphics.lineBetween(400, 300, 400 + this.point2.x, 300 + this.point2.y);
+
+        const dotProduct = this.point.dot(this.point2);
+
+        const area = this.point.length() * this.point2.length();
+
+        const angleBetween = Math.acos(dotProduct / area);
+
+        // only used to determine arc direction
+        const cross = this.point.cross(this.point2);
+
+        graphics.lineStyle(2, 0xaa0000);
+        graphics.beginPath();
+        graphics.arc(400, 300, 100, this.angle, this.angle + (cross < 0 ? -angleBetween : angleBetween));
+        graphics.strokePath();
+
+        this.text.setText([
+            'Dot product: ' + dotProduct,
+            'Normalized dot product: ' + dotProduct / area,
+            'Angle between vectors: ' + Phaser.Math.RadToDeg(angleBetween),
+            'Pointer is ' + (dotProduct > 0 ? 'in front of' : 'behind') + ' the blue vector direction'
+        ]);
     }
 }
 
 const config = {
-    type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: Example,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    }
+    type: Phaser.AUTO,
+    parent: 'game-container',
+    scene: Example
 };
 
 const game = new Phaser.Game(config);
